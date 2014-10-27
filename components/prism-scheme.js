5649725df37e498ed93596c28df3bc2b38a071d6
Nate Rosenbloom <nrosenbloom123@gmail.com>
2014-10-26 23:34:58 -0400

    added better handling for keywords & builtins, so that the 'define' in define-asdf (or whatever) wouldn't  parse as a keyword

--- components/prism-scheme.js
+++ components/prism-scheme.js
@@ -2,21 +2,21 @@ Prism.languages.scheme = {
     'boolean' : /#(t|f){1}/,
     'comment' : /;.*/,
     'keyword' : {
-	pattern : /([(|\s])(define(-syntax|-library|-values)?|(case-)?lambda|let(-values|(rec)?(\*)?)?|else|if|cond|begin|delay|delay-force|parameterize|guard|set!|(quasi-)?quote|syntax-rules)\b/,
+	pattern : /([(|\s])(define(-syntax|-library|-values)?|(case-)?lambda|let(-values|(rec)?(\*)?)?|else|if|cond|begin|delay|delay-force|parameterize|guard|set!|(quasi-)?quote|syntax-rules)\s/,
 	lookbehind : true
     },
     'builtin' : {
-	pattern :  /([(|\s])(cons|car|cdr|null\?|pair\?|boolean\?|eof-object\?|char\?|procedure\?|number\?|port\?|string\?|vector\?|symbol\?|bytevector\?|list|call-with-current-continuation|call\/cc|append|abs|apply|eval)\b/,
+	pattern :  /([(|\s])(cons|car|cdr|null\?|pair\?|boolean\?|eof-object\?|char\?|procedure\?|number\?|port\?|string\?|vector\?|symbol\?|bytevector\?|list|call-with-current-continuation|call\/cc|append|abs|apply|eval)\s/,
 	lookbehind : true
     },
     'string' :  /(["])(?:(?=(\\?))\2.)*?\1|'[^('|\s)]+/, //thanks http://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
     'number' : /(\s|\))[-+]?[0-9]*\.?[0-9]+((\s*)[-+]{1}(\s*)[0-9]*\.?[0-9]+i)?/,
     'operator' : {
-	pattern : /(?:[(|\s])(\*|\+|\-|\%|\/|<=|=>|>=|<|=|>)(?:[)|\s])/,
+	pattern : /(?:[(|\s])(\*|\+|\-|\%|\/|<=|=>|>=|<|=|>)/,
 	lookbehind : true
     },
      'function' : {
-	 pattern : /([(])[^(\s|))]+/,
+	 pattern : /([(])(\s*)[^(\s|))]+/,
 	lookbehind : true
     },
     'punctuation' : /[()]/
